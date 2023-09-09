b37bf476a5950c4d3b71acf12ac838e1248fc3e0
OlaJedrzejczyk <olajedrzejczyk16@gmail.com>
2023-09-09 11:42:03 +0200

    innitial commit

--- final_app/views.py
+++ final_app/views.py
@@ -1,10 +1,6 @@
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
+
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import RegistrationForm
 
 import requests
 import random
@@ -104,57 +100,3 @@ class ResultView(View):
     def get(self):
         return redirect('home_final_app')
 
-
-# def signup(request):
-#
-#     if request.method == "POST":
-#         form = RegistrationForm(request.POST)
-#         if form.is_valid():
-#             username = form.cleaned_data['username']
-#             first_name = form.cleaned_data['fname']
-#             last_name = form.cleaned_data['lname']
-#             email = form.cleaned_data['email']
-#             password = form.cleaned_data['pass1']
-#
-#             # create the user account
-#             user = User.objects.create_user(username=username, email=email, password=password)
-#             user.first_name = first_name
-#             user.last_name = last_name
-#             user.save()
-#
-#             messages.success(request, "Your account has been successfully created, now you can log in")
-#             return redirect('signin')
-#     else:
-#         form = RegistrationForm()
-#
-#     return render(request, 'signup.html')
-#
-#
-# def signin(request):
-#     all_genres = get_all_genres()
-#     if request.method == 'POST':
-#         username = request.POST['username']
-#         pass1 = request.POST['pass1']
-#
-#         user = authenticate(username=username, password=pass1)
-#
-#         if user is not None:
-#             login(request, user)
-#             # fname = user.first_name
-#             return redirect("form.html")
-#         else:
-#             messages.error(request, "Wrong details")
-#             return redirect('signin')
-#
-#     return render(request, "signin.html")
-#
-#
-# def signout(request):
-#     logout(request)
-#     # messages.success(request, "Logged out successfully")
-#     return redirect('home_final_app')
-
-
-# @login_required
-# def profile(request):
-#     return render(request, 'profile.html')
